933ed8aebce5dc3092bd4633af10f0c567710c97
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-04 09:49:31 +0200

    use creds for mongo connection

--- app/configs/conf.js
+++ app/configs/conf.js
@@ -1,15 +1,14 @@
-
 const devConf = {
-	db: "mongodb://localhost:27017/links",
-	listenPort: 3001,
-	jwtSecret: 'secret',
+    db: "mongodb://localhost:27017/links",
+    listenPort: 3001,
+    jwtSecret: 'secret',
 
 };
 
 const prodConf = {
-	db: "mongodb://mongo-links:27017/links",
-	listenPort: 8080,
-	jwtSecret: 'secret',
+    db: `mongodb://${env.MONGODB_USER}:${evn.MONGODB_PASSWORD}@mongo-links:27017/${MONGODB_DATABASE}`,
+    listenPort: 8080,
+    jwtSecret: 'secret',
 };
 exports.conf = devConf;
 if (process.env.PROFILE && process.env.PROFILE === "PROD") exports.conf = prodConf;
